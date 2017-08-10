6318af9accae118d3678d1495896825da497626b
TheOne-1 <alan.tan.tian@outlook.com>
2017-08-10 15:41:48 +0800

    upload all files

---
+++ app/src/main/java/com/example/alan/a0811whacamole/Controller.java
@@ -0,0 +1,13 @@
+package com.example.alan.a0811whacamole;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Controller extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_controller);
+    }
+}
